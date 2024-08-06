199c3f699ef2b37dc948323512f23aa8cb20ebce
Mirjalol <example@gmail.com>
2024-08-06 13:48:40 +0500

    topik project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import Navabr from './Components/Navbar/Navbar'
+import Navbar from './Components/Navbar/Navbar'
 
 function App() {
   return (
     <div>
-      <Navabr/>
+      <Navbar/>
+      <Section/>
     </div>
   )
 }
