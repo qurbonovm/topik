bab345f06e23313172f9fccb1e2d092139a5dcf5
Mirjalol <example@gmail.com>
2024-08-06 12:18:12 +0500

    topic project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navabr from './Components/Navbar/Navbar'
+
+function App() {
+  return (
+    <div>
+      <Navabr/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import  '../Navbar/Navbar.css'
+import Logo from '../Image/logo.png'
+
+function Navabr() {
+  return (
+
+      <div className="nav">
+        <div className="container">
+          <img className='nav-logo' src={Logo} alt="" />
+          <p className='nav-title'>Toshkentda joylashgan Koreys tili hamda Topik o’quv markazi</p>
+          <ul className='nav-item'>
+            <li className='nav-list'>Nega biz</li>
+            <li className='nav-list'>Natijalar</li>
+            <li className='nav-list'>Kurslar</li>
+            <li className='nav-list'>Ko’p beriladigan savollar</li>
+          </ul>
+          <div className="nav-phone">
+          <p className='nav-number'>+998 (33) 306 0098</p>
+          <p className='nav-data'>Hoziroq bog’laning</p>
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default Navabr 
